c6acdec0def5b44de7985f4d3b2160a9ba0dc217
Marshal <838720820@qq.com>
2018-02-27 18:44:16 +0800

    My spring fextival work
    I'm sorry for the delay.

--- Spring project/Monopoly4.py
+++ Spring project/Monopoly4.py
@@ -9,11 +9,14 @@ class ListNode :
       self.pointer=0
       self.whose =0
       self.cost = 0
+      self.previouspointer=0
 class Perinfor:
    def __init__(self):          # Init the map
       self.who= 0
       self.position=0
       self.money =sm
+      self.pointer =0
+      self.previouspointer =0
 
 def initilise():
     global List
@@ -28,19 +31,26 @@ def initilise():
     List = [ListNode() for i in range(nh)]  # Generate a list for the map
     for i in range(nh) :
        List[i].pointer = i + 1
+       List[i].previouspointer = i-1
        List[i].cost =random.randint(30,200)
     List[nh-1].pointer=0
+    List[0].previouspointer=nh-1
 
     Char = [Perinfor() for i in range(np)]    
     for i in range(np):
        Char[i].who=i+1
-
     #shuffle a list to generate random position for players
     l = list(range(0,nh))
     shuffle(l)
     for i in range(np):
         Char[i].position = l[i]
+        Char[i].pointer = i+2
+        Char[i].previouspointer =i
         List[l[i]].who=i+1
+        List[List[l[i]].previouspointer].pointer=List[l[i]].pointer
+        List[List[l[i]].pointer].previouspointer=List[l[i]].previouspointer
+    Char[0].previouspointer = np
+    Char[np-1].pointer=1
     # for lucky and bad luck
     List[l[nh-1]].whose="good luck"
     List[l[nh-2]].whose="bad  luck"
@@ -54,9 +64,12 @@ def Lucky(p):
        print("you get an extra monney of 1000 dollars")
        Char[p-1].money +=1000
     if l==2:
-        print("you get the luck to destory one builing, but I'm sorry, it is developing")
+        print("you get the luck to destory one builing")
+        d=int(input("Please choose the position of the place where you want to destroy"))
+        List[d].whose=0
     if l==3:
         print("you get the luck to move 2 more steps")
+        p=3-p
 
 def Unlucky(p):
     l=random.randint(1,3)
@@ -65,8 +78,15 @@ def Unlucky(p):
         Char[p-1].money-=700
     if l==2:
         print("you have to pay Marshal 10 rmb in order to continue the game")
+        print("please ask Marshal to enter the password after the payment")
+        password=int(input("Password:"))
+        if password!="123":
+            print("sorry")
+        
+            
     if l==3:
-        print("you have been kicked out of the room")
+        print("Yesterday, a thief came to your house and steal all you money ")
+        Char[p-1].money=0
     
        
 def playergo(p):
@@ -82,14 +102,10 @@ def playergo(p):
     print("The number of steps that you go is:",n)
     x=0   # Count the number of time that we go through
     while List[w].who==p and x!=n:
-         List[w].who=0
-         if List[List[w].pointer].who==0:
-             w=List[w].pointer
-             List[w].who=p
-         else:
-             w=List[List[w].pointer].pointer     #only allow two players
-             List[w].who=p
-             
+         List[w].who = 0
+         w=List[w].pointer
+         List[w].who=p
+
          x+=1
     Char[p-1].position=w
     if List[w].whose==0:
@@ -108,9 +124,7 @@ def Output():
         print(i, List[i].who, List[i].whose, List[i].cost)
     print("")
     for i in range(np):
-        print(i+1, Char[i].position, Char[i].money)
-        if Char[i].money<0:
-           print("Player", i+1, "has went bankrupt, all his house become empty")
+        print(i+1, Char[i].position, Char[i].money,Char[i].previouspointer, Char[i].pointer)
 
 def buy(p):
     cost=List[Char[p-1].position].cost
@@ -129,16 +143,37 @@ def upgrade(p):
             List[Char[p-1].position].cost*=2
 
 def pay(p,r):
-        c=List[Char[p-1].position].cost   #shortcut for the cost at that location
-        Char[p-1].money  -=c
-        Char[r-1].money  +=c
-        print("You have stepped onto the other player's area, you have to pay",c,"dollars") 
+    c=List[Char[p-1].position].cost   #shortcut for the cost at that location
+    Char[p-1].money  -=c
+    Char[r-1].money  +=c
+    print("You have stepped onto the other player's area, you have to pay",c,"dollars") 
 
 initilise()
 Output()
 p=1
-while Char[0].money>0 and Char[1].money>0:   #only for 2 playrs now
+def checkwin():
+    x=np
+    for i in range(np):
+        if Char[i].money <0:
+            print("Player", i+1, "has no money left, his houses are now free to buy")
+            Char[Char[i].previouspointer].pointer= Char[i].pointer   #remove the player
+            Char[Char[i].pointer].previouspointer= Char[i].previouspointer
+            for j in range(nh):
+                if List[j].who==i+1:
+                    List[j].who==0
+                if List[j].whose==i+1:
+                    List[j].whose=0    #make all his houses free
+        if Char[i].pointer==Char[i].who:
+            print("Player", i+1,"is the winner")
+            Onlyoneleft=True
+        
+                
+StartPointer=0
+Onlyoneleft=False
+while Onlyoneleft==False:   #only for 2 playrs now
     playergo(p)
-    p=3-p
+    p=Char[p-1].pointer
     Output()
+    checkwin()
+    
         
---
+++ Spring project/readme.txt
@@ -0,0 +1,6 @@
+I'm Marshal Zhang in Option1
+Spring festival improvements on <monopoly>
+
+During the spring festival, I improve my program by making multiplayer possible. So, player(s) will be able to choose the number of player. And also, the player can choose the size of the map. Above all, the new features that I put into my program allow player to DIY a lots of things. 
+
+I implemented the old way of changing players or positions, now everything is kept in linked list to ensure that everything goes on well. For example, when the direction of a player is reversed by the Lucky place or one of the player run out of money. Then everything will still work on perfectly. 
